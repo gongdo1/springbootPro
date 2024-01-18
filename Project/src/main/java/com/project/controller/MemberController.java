758f1765775932bd80448fcbc98750b56d85d8fc
pgd <pgd@DESKTOP-53KUUPF>
2024-01-19 00:11:36 +0900

    회원목록까지

--- Project/src/main/java/com/project/controller/MemberController.java
+++ Project/src/main/java/com/project/controller/MemberController.java
@@ -1,15 +1,50 @@
 package com.project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.project.account.AccountServiceImpl;
+import com.project.account.AccountVo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MemberController {
 
+	@Autowired
+	private AccountServiceImpl service;
 	
-	@GetMapping("index.do")
-	String index() {
-		System.out.println("맵핑TEST");
+	@GetMapping("/")
+	public String index() {
 		return "index";
 	}
+	@GetMapping("accountForm.do")
+	public String accountForm() {
+		return "/member/accountForm";
+	}
+	
+	@PostMapping("accountInsert.do")
+	public String accountInsert(AccountVo vo) {
+		System.out.println("vo= "+vo);
+		return "index";
+	}
+	
+	@GetMapping("accountList.do")
+	public String accountList(Model model, HttpServletRequest req, HttpSession session) {
+		System.out.println("TEST");
+
+		session.setAttribute("se", service.login(null));
+
+		model.addAttribute("li", service.accountList());
+		return "/member/accountList";
+	}
+	
+	
 }
--- Project/src/main/webapp/WEB-INF/views/index.jsp
+++ Project/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>TEST</h1>
+<h2>index</h2>
+<div>
+<a href="accountForm.do">회원가입</a>
+<a href="accountList.do">회원목록</a>
+${se.admin }
+</div>
 </body>
 </html>
